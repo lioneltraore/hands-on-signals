0a99f029ed2b4de74315b19c7d8483972b5b3010
Lionel Traore <hello@lioneltraore.com>
2024-07-21 13:55:52 +0000

    add tass style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserListComponent } from './ui/users/user-list/user-list.component';
+import { UserTodosComponent } from './ui/users/user-todos/user-todos.component';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserListComponent],
+  imports: [RouterOutlet, UserListComponent, UserTodosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  userService = inject(UserService);
+
   title = 'hands-on-signal';
 }
